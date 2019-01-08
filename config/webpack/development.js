18f44336ad80d541fad9ad3304ed227db632f82d
MasahiroTakahashi <take1931032@gmail.com>
2019-01-08 22:29:44 +0900

    webpacker設定してbootstrap入れてヘッダー作った

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,18 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
+// const { environment } = require('@rails/webpacker')
 const environment = require('./environment')
 
+// const webpack = require('webpack')
+
+// environment.plugins.prepend( 
+//   'Provide',
+//   new webpack.ProvidePlugin({
+//     jQuery: 'jquery/dist/jquery',
+//     Popper: 'popper.js/dist/popper'
+//   })
+// )
+
 module.exports = environment.toWebpackConfig()
+
+
