62c9dcbd42df9f592bf748425c7a3b6a7340e9b9
MasahiroTakahashi <take1931032@gmail.com>
2019-01-16 20:54:41 +0900

    今度こそ

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
+var webpack = require('webpack')
 const { environment } = require('@rails/webpacker')
+
+environment.plugins.prepend( 
+  'Provide',
+  new webpack.ProvidePlugin({
+    jquery: 'jquery/dist/jquery',
+    Popper: 'popper.js/dist/popper'
+  })
+)
 
 module.exports = environment
